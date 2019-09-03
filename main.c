a197d67f43c480a1237475505aaa9bdf758a6dc3
Jacob London <jelondon12@gmail.com>
2019-09-03 12:18:29 -0700

    updated header

--- main.c
+++ main.c
@@ -1,32 +1,25 @@
 #include "nce.h"
 
+void ncersize(int sig)
+{
+    getmaxyx(stdscr, SHeight, SWidth);
+}
+
+void nceaction()
+{
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+
+    for (int i = 0; i < SHeight; i++) {
+        for (int j = 0; j < SWidth; j++) {
+            swrite(chr, 'X', j, i, A_NORMAL);
+        }
+        swrite(str, "SUPER", 0, i, A_NORMAL);
+    }
+}
+
 int main()
 {
     nceinit();
-    start_color();
-    init_pair(1, COLOR_BLUE, COLOR_RED);
-    init_pair(2, COLOR_RED, COLOR_BLUE);
-
-    int ch;
-    for (;;) {
-        if ((ch = getch()) == ERR) {
-            /* user hasn't responded */
-        }
-        else {
-            switch (ch) {
-            case 'Q':
-                goto Exit;
-            }
-        }
-    }
-
-Exit:
-    endwin();
     return 0;
-}
-
-void ncersize(int sig)
-{
-    int nh, nw;
-    getmaxyx(stdscr, nh, nw);
 }
--- nce.h
+++ nce.h
@@ -3,7 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * User defined
+ */
+
+/* NCE Resize handler */
 void ncersize(int sig);
+/* NCE Action function */
+void nceaction();
+
+/**
+ * Internals
+ */
+
+void nceinit();
+void ncego();
+
+#define NUMKEYS 0xFF
+short Keys[NUMKEYS] = { 0 };
+int SWidth, SHeight;
+
+
 
 void nceinit()
 {
@@ -11,6 +31,50 @@ void nceinit()
     clear();
     cbreak();
     noecho();
+    getmaxyx(stdscr, SHeight, SWidth);
     signal(SIGWINCH, &ncersize);
-    nodelay(stdscr, TRUE);
+    nodelay(stdscr, true);
+    start_color();
+
+    ncego();
 }
+
+void ncego()
+{
+    int ch = 0;
+    for (;;) {
+        if ((ch = getch()) == ERR) {
+            /* pass */
+        } else {
+            /* set key to whether it was pressed or not */
+            for (short i = 0; i < NUMKEYS; i++)
+                Keys[i] = (ch == i);
+        }
+
+        if (Keys[27]) /* ESCAPE */
+            goto Exit;
+
+        nceaction();
+    }
+
+Exit:
+    endwin();
+}
+
+void _chrwrite(char ch, int x, int y)
+{
+    mvaddch(y, x, ch);
+}
+
+void _strwrite(char *s, int x, int y)
+{
+    move(y, x);
+    addstr(s);
+}
+
+/* Usage: writeattr(chr, 'a', 5, 10, A_BOLD); */
+#define swrite(type, v, x, y, attr) do { \
+    attron((attr)); \
+    _ ## type ## write((v), (x), (y)); \
+    attroff((attr)); \
+} while (0)
